a247507a71b1f2174245ec2c844bea10abab7e20
JBO <boldsja@gmail.com>
2022-03-31 01:23:47 -0500

    program start, folders added.

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+
+const askJob = () => {
+inquirer.prompt({
+    name: "job",
+    type: "list",
+    choices: ["Intern","Employee", "Engineer", "Manager"],
+    message:"Choose Job Title"
+  })
+}
+
+//initate program
+askJob();
