54a5c1271f527e84eb2ed5dc3009d5b597df7e65
JBO <boldsja@gmail.com>
2022-03-31 16:15:34 -0500

    categories added, templates added.

--- index.js
+++ index.js
@@ -1,16 +1,94 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/manager");
+const path = require("path");
+const fs = require("fs");
+
+const teamArray = [];
+const DIST_DIR = path.resolve(__dirname, "dist");
+const output_path = path.join(DIST_DIR, "team.html");
+
+const template = require("./src/page-template.js")
 
 
-const askJob = () => {
-inquirer.prompt(
-  {
-    name: "job",
-    type: "list",
-    choices: ["Intern","Employee", "Engineer", "Manager"],
-    message:"Choose Job Title"
+
+function mainMenu() {
+  function createManager() {
+
+    inquirer.prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is the manager's name?"
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "What is the manager's ID?"
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "What is the manager's email?"
+      },
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "What is the manager's office number?"
+      },
+    ]).then((response)=>{
+      const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
+      teamArray.push(manager)
+      askJob()
+    })
   }
-)}
+
+  const askJob = () => {
+    inquirer.prompt(
+      {
+        name: "job",
+        type: "list",
+        choices: ["Intern", "Engineer", "Manager", "I dont want to add anymore team members"],
+        message: "What kind of employee would you like to add?"
+      }
+    ).then((response)=>{
+      // if(response.job === "Manager"){
+      //   createManager()
+      // }
+      switch(response.job){
+        case "Engineer":
+          // createEngineer()
+          break;
+        case "Manager":
+          createManager()
+          break;
+        case "Intern":
+          // createIntern()
+          break;
+          default:
+            buildTeam()
+      }
+    })
+  }
+
+  function buildTeam(){
+    if(!fs.existsSync(DIST_DIR)){
+      fs.mkdirSync(DIST_DIR)
+    }
+    fs.writeFileSync(output_path, template(teamArray), 'utf-8')
+  }
+
+  createManager();
+}
+
+mainMenu();
 
 
 //initate program
-askJob();
+//askJob();
+
+// fs.writeFile('readme-gen.md', data, err => {
+//     if (err) {
+//       console.error(err)
+//       return
+//     }
+// })
---
+++ src/page-template.js
@@ -0,0 +1,21 @@
+const generateTeam = team => {
+    console.log("team from index.js",team)
+
+// in here you will have three different functions to generate your manager, engineer, and intern
+
+// youre going to write some logic that filters through each team member depending on their getRole() method which will return their respective role...
+// and you will send them to that speciic function to generate them
+
+// split the team by filtering them via getRole()
+// map over everyone that passes that filter and push them to their respective function
+// after everyone is rendered youre going to call you generateTeam() function in an html template below as a module.exports
+
+
+
+
+
+}
+
+generateTeam()
+
+module.exports = generateTeam
