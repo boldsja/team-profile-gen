b6ac8839bc82e5145c8dff6e16ea7688d79f1ab1
JBO <boldsja@gmail.com>
2022-04-20 07:36:01 -0500

    updated

--- src/page-template.js
+++ src/page-template.js
@@ -71,9 +71,20 @@ module.exports = team => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/flatly/bootstrap.min.css" />
-        <title>Your Team</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="../src/style.css"/>
+        <title>Team Generator</title>
     </head>
     <body>
+      <header>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Your Generated Team</h1>
+            <p class="lead">Find information about each member below.</p>
+          </div>
+        </div>
+      </header>
+
     <div>
         <div class='row'>
     ${generateTeam(team)}
